545dc3947ee423e83e26992aa5559a7afb7a9125
George Zhang <gzhang01@college.harvard.edu>
2016-11-22 00:25:54 -0500

    Finished gameplay

--- blackjack/game/game.py
+++ blackjack/game/game.py
@@ -16,7 +16,8 @@ class Blackjack:
 	def __init__(self, players=[Player()], numDecks=8, **kwargs):
 		self.numDecks = numDecks
 		self.deck = BlackjackDeck(self.numDecks)
-		self.players = list(players)
+		# [player, bet]
+		self.players = [[p, 0] for p in players]
 		self.dealer = Player()
 		self.reshuffle = False
 
@@ -36,36 +37,99 @@ class Blackjack:
 			card = self.deck.drawCard()
 		player.addToHand(card)
 
-	#### TODO: Think about how to notify players that deck has been reshuffled
-
 	# Deals cards to start a round
 	def startRound(self):
 		for _ in xrange(2):
 			for p in self.players:
-				self.dealCard(p)
+				self.dealCard(p[0])
 			self.dealCard(self.dealer)
+
+	def printUpcard(self):
+		print "\n"
+		print "Dealer upcard: {0}".format(self.dealer.getHand()[0])
 
 	# Runs the game
 	def playRound(self):
 		self.startRound()
 
+		# Gather bets
+		for p in self.players:
+			bet = p[0].getBet(self.noPrint)
+			p[1] = bet
+
 		# Players take turns
 		for p in self.players:
-			while True: 
+			while True:
 				if not self.noPrint:
-					print "\n"
-					print "Dealer upcard: {0}".format(self.dealer.getHand()[0])
-				action = p.getAction(self.noPrint)
+					self.printUpcard()
+				action = p[0].getAction(self.noPrint)
 
-				if action == "stand":
+				# If bust, zero out bet
+				if action == "bust":
+					p[1] = 0
+					break
+				elif action == "stand":
 					break
 				elif action == "hit":
-					self.dealCard(p)
+					self.dealCard(p[0])
 
-				# TODO: list of active players for busting?
+		# Dealer actions
+		while True:
+			dealerValue = self.dealer.getHandValue()[0]
+			if not self.noPrint:
+				print "\n"
+				print "Dealer hand: " + " ".join([str(card) for card in self.dealer.getHand()])
+				print "Dealer hand value: {0}".format(dealerValue)
+				if dealerValue > 21:
+					print "Dealer BUST"
+				elif dealerValue >= 17:
+					print "Dealer STANDS on {0}".format(dealerValue)
+				else:
+					print "Dealer HITS"
+
+			# Dealer stands on 17
+			if dealerValue >= 17:
+				break
+
+			# Else hit
+			self.dealCard(self.dealer)
+
+		# Determine winnings
+		for p in self.players:
+			playerValue = p[0].getHandValue()[0]
+
+			if not self.noPrint:
+				print "\n"
+				print "Dealer has value {0} and you have value {1}".format(dealerValue, playerValue)
+
+			if playerValue == "Blackjack":
+				p[0].addMoney(5 * p[1] / 2)
+				if not self.noPrint: print "You win ${0}".format(3 * p[1] / 2)
+			elif dealerValue > 21 or playerValue > dealerValue:
+				p[0].addMoney(2 * p[1])
+				if not self.noPrint: print "You win ${0}".format(p[1])
+			elif playerValue == dealerValue:
+				p[0].addMoney(p[1])
+				if not self.noPrint: print "Push"
+			else:
+				p[0].addMoney(0)
+				if not self.noPrint: print "You lose {0}".format(p[1])
+
+		# Clear cards
+		for p in self.players:
+			p[0].discardHand()
+		self.dealer.discardHand()
+
+		# Reshuffle if needed:
+		if self.reshuffle:
+			#### TODO: Think about how to notify players that deck has been reshuffled
+			print "Reshuffling!"
+			self.deck.reshuffle()
+			self.reshuffle = False
 
 
 
 if __name__ == "__main__":
 	game = Blackjack()
-	game.playRound()
+	while True:
+		game.playRound()
