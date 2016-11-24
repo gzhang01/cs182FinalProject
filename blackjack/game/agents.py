7a6d4f5f692b365d03f303a7b708a3025cbf1156
akangaroo <akamy7@gmail.com>
2016-11-24 16:11:19 -0600

    adding file for learning agents

---
+++ blackjack/game/agents.py
@@ -0,0 +1,20 @@
+####
+ # agents.py
+ #
+ # Learning agents for blackjack
+ #
+ # CS182 - Artificial Intelligence
+ # Fall 2016
+ # Final Project 
+ ##
+
+from card import Card 
+from deck import BlackjackDeck
+from player import Player 
+import constants as const
+
+class RandomAgent:
+	# Initializes poker card with given value and suit
+	def __init__(self):
+	
+
