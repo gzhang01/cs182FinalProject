7469645ef6f3a69deeed15ea4ad22d2c55213630
George Zhang <gzhang01@college.harvard.edu>
2016-11-30 20:54:28 -0500

    New data for Presentation

--- data/plotData.py
+++ data/plotData.py
@@ -12,8 +12,9 @@ with open(infile, "r") as f:
 	lines = f.readlines()
 lines = [int(line.strip()) for line in lines]
 
-plt.plot(lines)
-plt.suptitle('Q Learning Agent (bet = 10)', fontsize=20)
+color = "g"
+plt.plot(lines, color)
+plt.suptitle('Basic Strategy Agent (bet = 10)', fontsize=20)
 plt.ylabel('Money ($)')
 plt.xlabel('Iteration')
 plt.savefig(outfile)
