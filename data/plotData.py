13622a53f8f16c333f7de097f569d8992faf0d9c
George Zhang <gzhang01@college.harvard.edu>
2016-12-05 17:21:01 -0500

    More data

--- data/plotData.py
+++ data/plotData.py
@@ -12,7 +12,7 @@ with open(infile, "r") as f:
 	lines = f.readlines()
 lines = [int(line.strip()) for line in lines]
 
-color = "g"
+color = "r"
 plt.plot(lines, color)
 plt.suptitle('Basic Strategy Agent (bet = 10)', fontsize=20)
 plt.ylabel('Money ($)')
